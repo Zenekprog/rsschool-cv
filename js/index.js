a0c8b87705d2c88a405f55fcd7347a32750eac82
zenekprog <zenekprog@gmail.com>
2022-06-14 22:49:42 +0300

    feat: add nav-menu

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const burger=document.querySelector(".burger")
+const nav_menu=document.querySelector(".nav-menu")
     burger.onclick=()=>{
         burger.classList.toggle("active-burger");
+        nav_menu.classList.toggle("nav-menu-active")
     }
