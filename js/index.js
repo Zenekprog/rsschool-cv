cbd08ef66124358eb17634f997f5a2e25c58ee02
zenekprog <zenekprog@gmail.com>
2022-06-15 22:50:55 +0300

    feat: add overflow in menu add script

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-const burger=document.querySelector(".burger")
-const nav_menu=document.querySelector(".nav-menu")
+const burger=document.querySelector('.burger')
+const nav_menu=document.querySelector('.nav-menu')
+const body = document.querySelector('body')
+
     burger.onclick=()=>{
-        burger.classList.toggle("active-burger");
-        nav_menu.classList.toggle("nav-menu-active")
+        burger.classList.toggle('active-burger');
+        nav_menu.classList.toggle('nav-menu-active')
+        body.classList.toggle('lock')
+    }
+
+    nav_menu.onclick=()=>{
+        nav_menu.classList.remove('nav-menu-active')
+        burger.classList.remove('active-burger');
+        body.classList.remove('lock')
     }
